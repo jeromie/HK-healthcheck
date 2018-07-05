e30cb909b531a5bd79d1b8ee520e41a589c12407
Ajaj Rajguru <ajaj@ajency.in>
2018-07-05 18:54:00 +0530

    scroll to section js added

---
+++ content/steps.md
@@ -0,0 +1,16 @@
+---
+title: "Steps"
+date: 2018-06-04T12:57:20+05:30
+draft: false
+layout: steps
+url: /steps/
+
+steps_status: "done active"
+
+step1_status: "current"
+step2_status: "current"
+step3_status: "current"
+
+step1_button: true
+
+---
--- themes/HK/static/js/custom.js
+++ themes/HK/static/js/custom.js
@@ -6,4 +6,39 @@ $(function(){
 		$('.success-data').removeClass('d-none');
 	});
 
+	// Scroll to id on load
+	function scrollToID(){
+		var id = window.location.hash;
+	    // target element
+	    var $id = $(id);
+	    if ($id.length === 0) {
+	        return;
+	    }
+	    // top position relative to the document
+	    var pos = $id.offset().top - 120;
+	    
+	    // animated top scrolling
+		$('html, body').animate({ scrollTop: pos }, 'slow');
+	}
+
+	scrollToID();
+
+	// Scroll to id when click
+
+	$(document).on('click', 'button[data-href^="#"]', function(e) {
+	   var id = $(this).attr('data-href');
+	    // target element
+	    var $id = $(id);
+	    if ($id.length === 0) {
+	        return;
+	    }
+
+	    // top position relative to the document
+	    var pos = $id.offset().top - 120;
+    
+	    // animated top scrolling
+		$('html, body').animate({scrollTop: pos}, 'slow');
+
+	});
+
 });
