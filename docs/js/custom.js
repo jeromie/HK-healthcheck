d7b64313308ddf00d6ad9d72807105a981fd50c0
Ajaj Rajguru <ajaj@ajency.in>
2018-07-05 19:03:09 +0530

    build

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -6,4 +6,40 @@ $(function(){
 		$('.success-data').removeClass('d-none');
 	});
 
+	// Scroll to id on load
+	function scrollToID(){
+		var id = window.location.hash;
+	    // target element
+	    var $id = $(id);
+	    if ($id.length === 0) {
+	        return;
+	    }
+	    // top position relative to the document
+	    var pos = $id.offset().top - 120;
+	    
+	    // animated top scrolling
+		$('html, body').animate({ scrollTop: pos }, 'slow');
+	}
+
+	scrollToID();
+
+	// Scroll to id when click
+
+	$(document).on('click', '[data-href^="#"]', function(e) {
+	   var id = $(this).attr('data-href');
+	    // target element
+	    var $id = $(id);
+	    if ($id.length === 0) {
+	        return;
+	    }
+
+	    e.preventDefault();
+	    // top position relative to the document
+	    var pos = $id.offset().top - 120;
+    
+	    // animated top scrolling
+		$('html, body').animate({scrollTop: pos}, 'slow');
+
+	});
+
 });
