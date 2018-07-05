ca15713503d73f581b95af7cb35704f61fdec779
Ajaj Rajguru <ajaj@ajency.in>
2018-07-05 16:25:27 +0530

    Success modal + custom js file created

---
+++ themes/HK/static/js/custom.js
@@ -0,0 +1,9 @@
+$(function(){
+
+	// Success data on confirm
+	$('.show-success').click(function(){
+		$('.confirm-data').addClass('d-none');
+		$('.success-data').removeClass('d-none');
+	});
+
+});
