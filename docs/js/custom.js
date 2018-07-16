38f0ad429dcc3d9d17fea352a7deedb451a2f78e
jeromie <jeromie@ajency.in>
2018-07-16 12:21:42 +0530

    changes

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -9,35 +9,33 @@ $(function(){
 	// Scroll to id on load
 	function scrollToID(){
 		var id = window.location.hash;
-	    // target element
-	    var $id = $(id);
-	    if ($id.length === 0) {
-	        return;
-	    }
-	    // top position relative to the document
-	    var pos = $id.offset().top - 120;
+		// target element
+		var $id = $(id);
+		if ($id.length === 0) {
+			return;
+		}
+		// top position relative to the document
+		var pos = $id.offset().top - 120;
 
-	    // animated top scrolling
+		// animated top scrolling
 		$('html, body').animate({ scrollTop: pos }, 'slow');
 	}
-
 	scrollToID();
 
 	// Scroll to id when click
-
 	$(document).on('click', '[data-href^="#"]', function(e) {
 	   var id = $(this).attr('data-href');
-	    // target element
-	    var $id = $(id);
-	    if ($id.length === 0) {
-	        return;
-	    }
+		// target element
+		var $id = $(id);
+		if ($id.length === 0) {
+			return;
+		}
 
-	    e.preventDefault();
-	    // top position relative to the document
-	    var pos = $id.offset().top - 120;
+		e.preventDefault();
+		// top position relative to the document
+		var pos = $id.offset().top - 120;
 
-	    // animated top scrolling
+		// animated top scrolling
 		$('html, body').animate({scrollTop: pos}, 'slow');
 
 	});
@@ -46,13 +44,25 @@ $(function(){
 	$(document).on('click', '.print-report', function(e) {
 		var element = document.getElementById('printform');
 		var opt = {
-		  margin:       0.25,
-		  filename:     'HK-Report.pdf',
-		  image:        { type: 'jpeg', quality: 0.98 },
-		  html2canvas:  { scale: 2 },
-		  jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+			margin:       0.25,
+			filename:     'HK-Report.pdf',
+			image:        { type: 'jpeg', quality: 0.98 },
+			html2canvas:  { scale: 2 },
+			jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
 		};
 		html2pdf().from(element).set(opt).save();
+	});
+
+	// Show/hide other location
+	$("select.location-select").change(function(){
+		var selectedLocation = $(".location-select option:selected").val();
+		if (selectedLocation == "other"){
+			$(".other-location").show();
+			$(".other-location").find("input").attr("required", "true");
+		} else {
+			$(".other-location").hide();
+			$(".other-location").find("input").removeAttr("required");
+		}
 	});
 
 });
